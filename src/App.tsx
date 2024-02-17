bf255dc645e510831d8c77e7786609428c5492ac
mohammad <mohammad.farhadi@bimobject.com>
2024-02-17 15:03:04 +0100

    #style: add testimonials section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import './App.css'
-import Part1 from "./components/Part1.tsx";
-import Part2 from "./components/Part2.tsx";
-import Part3 from "./components/Part3.tsx";
+import Testimonials from "./components/Testimonials.tsx";
 
 function App() {
 
     return (
         <>
-            <Part3/>
-            <Part2/>
-            <Part1/>
+            {/*<Part3/>*/}
+            {/*<Part2/>*/}
+            {/*<Part1/>*/}
+            <Testimonials/>
         </>
     )
 }
